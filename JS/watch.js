e50285294850b2f67f9ac8008d7fdc34404c631c
Robert Rappole <rappole4@yahoo.com>
2017-01-17 14:15:04 -0500

    after one pm fix

--- JS/watch.js
+++ JS/watch.js
@@ -11,9 +11,13 @@ document.addEventListener("DOMContentLoaded",function(){
 	function zero(i) {
 		if (i<10){
 			return "0"+i;
-		}
-		 return i;
+		} return i;
 	}
+	function over(m) {
+		if (m>12) {
+			return m-12;
+		} return m;
+	}	
 setInterval(function(){
 	if (one.innerText==12&&two.innerText==59&&three.innerText==59){
 			one.innerText="0"+1;
@@ -23,15 +27,16 @@ setInterval(function(){
 	}	else if (two.innerText==59&&three.innerText==59){
 			two.innerText="0"+0;
 			three.innerText="0"+0;
-			one.innerText =parseInt(one.innerText) +1;
+			one.innerText =over(parseInt(one.innerText) +1);
 			body.style.backgroundColor = "#"+parseInt(one.innerText)+"0000";
 	}	else if (three.innerText==59){
 			three.innerText="0"+0;
 			two.innerText =zero(parseInt(two.innerText) + 1);
+			one.innerText =over(parseInt(one.innerText) + 0);
 			body.style.backgroundColor = "#"+parseInt(one.innerText)+parseInt(two.innerText)+"00";
 	}	else (three.innerText=zero(parseInt(three.innerText)+1))
 			zero(parseInt(two.innerText));
-			parseInt(one.innerText);
+			over(parseInt(one.innerText));
 			body.style.backgroundColor = "#"+zero(parseInt(one.innerText))+zero(parseInt(two.innerText))+zero(parseInt(three.innerText));
 },1000)
 });
